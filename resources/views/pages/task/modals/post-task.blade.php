2e43c4b80f7f0a967a84547a5f7b30e706e4bac5
Ervinne Sodusta <ervinne.sodusta@nuworks.ph>
2017-12-06 11:29:16 +0800

    Add Post task modal

---
+++ resources/views/pages/task/modals/post-task.blade.php
@@ -0,0 +1,43 @@
+<div class="modal fade">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Create Post for Task</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form id="group-form" class="fields-container">
+
+          {{ csrf_field() }}
+
+          <div class="row">
+              <div class="col-md-12">
+                  <div class="form-group">
+                      <label class="control-label">Task To Post (TODO: add field names)</label>
+                      <select name="task_id" required class="form-control select2-input"></select>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="control-label">Post Text</label>
+                      <textarea id="new-post-textarea" class="form-control" placeholder="Text to display in the post"></textarea>
+                  </div>
+
+                  <div class="form-group">
+                      <label class="control-label" >Deadline</label>
+                      <input name="date_time_to" type="text" class="form-control">
+                  </div>
+
+              </div>            
+          </div> 
+
+      </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
